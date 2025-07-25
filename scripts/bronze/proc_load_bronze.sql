d7bba462dda71ed179d0f3135699c185be37b97b
Ahmad Firas <144866382+Firas-Faisal@users.noreply.github.com>
2025-07-25 18:44:58 +0800

    Add naming conventions documentation and SQL scripts for Gold layer
    - Created a new documentation file outlining naming conventions for schemas, tables, views, and columns in the data warehouse.
    - Added DDL script to create views for the Gold layer, including dimension and fact tables.
    - Implemented a testing script to load all tables from Bronze and Silver layers.
    - Developed quality check scripts for the Gold layer to validate data integrity and consistency.
    - Created quality check scripts for the Silver layer to ensure data accuracy and standardization.
    - Added a troubleshooting script to assist in checking database objects and schemas.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,184 +1,141 @@
 /*
-
-=====================================================================
+===============================================================================
 Stored Procedure: Load Bronze Layer (Source -> Bronze)
-=====================================================================
+===============================================================================
 Script Purpose:
-	This stored procedure loads data into the 'bronze' schema from external csv file.
-	It performs the following actions:
-	- Truncate the bronze tables before loading data
-	- Uses the 'BULK INSERT' commmand to load data from csv Files to bronze tables.
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
 Parameters:
-	None.
-  This stored procedure does not accept any parameters or return any values.
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-	EXEC bronze.load_bronze;
-=====================================================================
-*/
--- GO is used to separate SQL batches in SSMS (SQL Server Management Studio)
-GO
-
-/* 
-CREATE OR ALTER will create the stored procedure if it doesn't exist, 
-or update it if it does � without needing a DROP PROCEDURE.
+    EXEC bronze.load_bronze;
+===============================================================================
 */
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME ;
-
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
 	BEGIN TRY
 		SET @batch_start_time = GETDATE();
-		-- Logging start of Bronze Layer load
-		PRINT '=======================================';
+		PRINT '================================================';
 		PRINT 'Loading Bronze Layer';
-		PRINT '=======================================';
+		PRINT '================================================';
 
-		-- SECTION: CRM DATA --
-		PRINT '---------------------------------------';
+		PRINT '------------------------------------------------';
 		PRINT 'Loading CRM Tables';
-		PRINT '---------------------------------------';
+		PRINT '------------------------------------------------';
 
-		-- TABLE: bronze.crm_cust_info --
-		/* 
-		Kosongkan semua data dalam table crm_cust_info sebelum load data baru.
-		TRUNCATE lebih cepat dan jimat log berbanding DELETE.
-		*/
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_cust_info';
 		TRUNCATE TABLE bronze.crm_cust_info;
-
-		-- Load CSV data using BULK INSERT
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\offic\Documents\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
 		WITH (
-			FORMAT = 'CSV',
-			FIRSTROW = 2, -- Skip header row
+			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '--------------------------------------';
-		-- TABLE: bronze.crm_prd_info --
-		SET @start_time = GETDATE();
+		PRINT '>> -------------';
+
+        SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\offic\Documents\GitHub\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_crm\prd_info.csv'
 		WITH (
-			FORMAT = 'CSV',
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '--------------------------------------';
-		-- TABLE: bronze.crm_sales_details --
-		SET @start_time = GETDATE();
+		PRINT '>> -------------';
+
+        SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.crm_sales_details;
-
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\offic\Documents\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_crm\sales_details.csv'
 		WITH (
-			FORMAT = 'CSV',
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '--------------------------------------';
-		-- SECTION: ERP DATA --
-		PRINT '---------------------------------------';
+		PRINT '>> -------------';
+
+		PRINT '------------------------------------------------';
 		PRINT 'Loading ERP Tables';
-		PRINT '---------------------------------------';
-
-		-- TABLE: bronze.erp_cust_az12 --
-		SET @start_time = GETDATE();
-
-		PRINT '>> Truncating Table: bronze.erp_cust_az12';
-		TRUNCATE TABLE bronze.erp_cust_az12;
-
-		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\offic\Documents\GitHub\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-		WITH (
-			FORMAT = 'CSV',
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '--------------------------------------';
-		-- TABLE: bronze.erp_loc_a101 --
+		PRINT '------------------------------------------------';
+		
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.erp_loc_a101';
 		TRUNCATE TABLE bronze.erp_loc_a101;
-
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\offic\Documents\GitHub\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_erp\loc_a101.csv'
 		WITH (
-			FORMAT = 'CSV',
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '--------------------------------------';
-		-- TABLE: bronze.erp_px_cat_g1v2 --
+		PRINT '>> -------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\sql\dwh_project\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\offic\Documents\GitHub\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
-			FORMAT = 'CSV',
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '--------------------------------------';
+		PRINT '>> -------------';
+
 		SET @batch_end_time = GETDATE();
-		PRINT '=======================================';
-		PRINT 'Bronze Layer Load - COMPLETE';
-		PRINT 'Total Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-		PRINT '=======================================';
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
 	END TRY
-
 	BEGIN CATCH
-		-- If error occurs, log the error info
-		PRINT '=======================================';
-		PRINT 'ERROR OCCURRED DURING LOADING BRONZE LAYER';
-
-		/* 
-		ERROR_MESSAGE()    - Tunjuk mesej error SQL Server.
-		ERROR_NUMBER()     - Nombor error yang spesifik.
-		ERROR_STATE()      - Status error (berguna untuk debug).
-		*/
-		PRINT 'Error Message: ' + ERROR_MESSAGE();
-		PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR);
-		PRINT '=======================================';
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
 	END CATCH
-
-END;
-GO
--- Execute the stored procedure
-
-EXEC bronze.load_bronze;
+END
--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -16,15 +16,15 @@ WARNING:
 USE master;
 GO
 
---Drop and recreate the 'DataWarehouse' database
-IF EXISTS(SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DataWarehouse;
 END;
 GO
---Create Database 'DataWarehouse'
 
+-- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
 GO
 
@@ -40,5 +40,3 @@ GO
 
 CREATE SCHEMA gold;
 GO
-
-
